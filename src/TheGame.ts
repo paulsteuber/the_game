3a965f8abc50b5c679ee3cf6237b39367ceedd25
Paul Steuber <paul.steuber@gmail.com>
2022-11-20 17:37:42 +0100

    some changes

--- src/TheGame.ts
+++ src/TheGame.ts
@@ -1,11 +1,11 @@
 import { Game, Player, Stack } from "./types";
-import CardHelper from "./utilities/CardHelper"
-import PlayHelper from "./utilities/PlayHelper"
+import CardHelper from "./utilities/CardHelper";
+import PlayHelper from "./utilities/PlayHelper";
 
-export default class TheGame{
-  constructor(){}
+export default class TheGame {
+  constructor() {}
 
-  static getNewGame(): Game{
+  static getNewGame(): Game {
     return {
       status: {
         allowUserToPlay: true,
@@ -13,98 +13,99 @@ export default class TheGame{
       initialized: false,
       players: [],
       stacks: [
-        {id: 0, up: true, cards:[]},
-        {id: 1, up: true, cards:[]},
-        {id: 2, up: false, cards:[]},
-        {id: 3, up: false, cards:[]},
+        { id: 0, up: true, cards: [] },
+        { id: 1, up: true, cards: [] },
+        { id: 2, up: false, cards: [] },
+        { id: 3, up: false, cards: [] },
       ],
-      refillStack: CardHelper.mix([ ...Array(98).keys() ].map( i => i+2))  
-    }
+      refillStack: CardHelper.mix([...Array(98).keys()].map((i) => i + 2)),
+    };
   }
-  static initPlayers(playerCount: number): Player[]{
-    let players: Player [] = [];
-    for(let i = 0; i < playerCount; i++){
+  static initPlayers(playerCount: number): Player[] {
+    let players: Player[] = [];
+    for (let i = 0; i < playerCount; i++) {
       let newPlayer = {
-        cards:[]
-      }
+        cards: [],
+      };
       players.push(newPlayer);
     }
     return players;
   }
-  static givePlayersCards(game: Game): Game{
-     const countCards: number = CardHelper.cardsPerPlayer(game.players.length);
-     console.log("Playyyyyaas", game.players)
-     //give all players their first cards
-     for(let p = 0; p < game.players.length; p++){
-       console.log("Player ",p);
-       let cards: number [] = [];
-       for (let c = 0; c < countCards; c++){
-         const shiftedNum: number | undefined = game.refillStack.shift();
-         if (!shiftedNum) break;
-         game.players[p].cards.push(shiftedNum);
-       }
-       game.players[p].cards = CardHelper.sortCards(game.players[p].cards);
-     }
-     console.log("GAME", game)
-     return game;
+  static givePlayersCards(game: Game): Game {
+    const countCards: number = CardHelper.cardsPerPlayer(game.players.length);
+    console.log("Playyyyyaas", game.players);
+    //give all players their first cards
+    for (let p = 0; p < game.players.length; p++) {
+      console.log("Player ", p);
+      let cards: number[] = [];
+      for (let c = 0; c < countCards; c++) {
+        const shiftedNum: number | undefined = game.refillStack.shift();
+        if (!shiftedNum) break;
+        game.players[p].cards.push(shiftedNum);
+      }
+      game.players[p].cards = CardHelper.sortCards(game.players[p].cards);
+    }
+    return game;
   }
 
-  static isCardAllowed(cardValue: number, stack: Stack):boolean{
-    if(!stack.cards.length) return true;
-    const lastStackNumber:number = stack.cards[stack.cards.length -1];
-    if(stack.up){
-      return lastStackNumber < cardValue  || lastStackNumber -10 === cardValue
+  static isCardAllowed(cardValue: number, stack: Stack): boolean {
+    if (!stack.cards.length) return true;
+    const lastStackNumber: number = stack.cards[stack.cards.length - 1];
+    if (stack.up) {
+      return lastStackNumber < cardValue || lastStackNumber - 10 === cardValue;
     }
-    return lastStackNumber > cardValue || lastStackNumber +10 === cardValue
+    return lastStackNumber > cardValue || lastStackNumber + 10 === cardValue;
   }
-  static isCardTenBagger(cardValue: number, stack: Stack):boolean{
-    const lastStackNumber:number = stack.cards[stack.cards.length -1];
-    if(!stack.cards.length) return false;
-    if(stack.up){
-      return lastStackNumber -10 === cardValue
+  static isCardTenBagger(cardValue: number, stack: Stack): boolean {
+    const lastStackNumber: number = stack.cards[stack.cards.length - 1];
+    if (!stack.cards.length) return false;
+    if (stack.up) {
+      return lastStackNumber - 10 === cardValue;
     } else {
-      return lastStackNumber +10 === cardValue
+      return lastStackNumber + 10 === cardValue;
     }
   }
 
   static isAllowedFinishMove(game: Game): boolean {
     //if refillstack is not empty player must play minimum 2 cards
-    const minimumPlayedCards: number  = game.refillStack.length ? 2: 1;
-    const cardsPerPlayer: number = CardHelper.cardsPerPlayer(game.players.length);
-    if(game.players[0].cards.length <= cardsPerPlayer - minimumPlayedCards){
+    const minimumPlayedCards: number = game.refillStack.length ? 2 : 1;
+    const cardsPerPlayer: number = CardHelper.cardsPerPlayer(
+      game.players.length
+    );
+    if (game.players[0].cards.length <= cardsPerPlayer - minimumPlayedCards) {
       return true;
     }
     return false;
-    
   }
 
-  static otherPlayersPlay(game: Game):Game{
-    let otherPlayers = game.players.filter((player, index ) => index != 0);
-    
+  static otherPlayersPlay(game: Game): Game {
+    let otherPlayers = game.players.filter((player, index) => index != 0);
+
     otherPlayers.forEach((player, index) => {
-      let wannaPlay: [{card: number, toStackId: number}] | [];
+      let wannaPlay: [{ card: number; toStackId: number }] | [];
       const playHelper = new PlayHelper(player.cards, game.stacks);
 
       wannaPlay = playHelper.getCardsToPlay();
-      if(wannaPlay.length){
-        wannaPlay.forEach((wp) =>{
-          console.log("WP",wp)
-          setTimeout(()=>{
+      if (wannaPlay.length) {
+        wannaPlay.forEach((wp) => {
+          console.log("WP", wp);
+          setTimeout(() => {
             //remove card from player cards array
-            game.players[index+1].cards = player.cards.filter((card) => card !== wp.card);
+            game.players[index + 1].cards = player.cards.filter(
+              (card) => card !== wp.card
+            );
             //add card to stack
-            console.log("XXX",game.stacks)
+            console.log("XXX", game.stacks);
             game.stacks[wp.toStackId].cards.push(wp.card);
-            console.log(`Player ${index+1} played Card ${wp.card} to Stack ${wp.toStackId}`)
-          },1000)
+            console.log(
+              `Player ${index + 1} played Card ${wp.card} to Stack ${
+                wp.toStackId
+              }`
+            );
+          }, 1000);
         });
       }
-      
-      
-
-
     });
     return game;
   }
 }
-
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,51 +1,92 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GameContext } from "../GameContext";
 import TheGame from "../TheGame";
 
-export function UserCard(cardNumber:{value:number}){
-  const { gameStore, setGameStore  } = useContext<any>(GameContext);
-  const [buttonStatus, setButtonStatus] = useState([{className: "btn-light"},{className: "btn-light"},{className: "btn-light"},{className: "btn-light"}]);
+export function UserCard(cardNumber: { value: number }) {
+  const { gameStore, setGameStore } = useContext<any>(GameContext);
+  const [buttonStatus, setButtonStatus] = useState([
+    { className: "btn-light" },
+    { className: "btn-light" },
+    { className: "btn-light" },
+    { className: "btn-light" },
+  ]);
 
+  useEffect(() => {
+    console.log("huhu");
+  });
   const addCardToStack = (stackId: number) => {
-    const game = {...gameStore};
+    const game = { ...gameStore };
     const selectedStack = game.stacks[stackId];
     /** check if user is allowed to add this number */
-    console.log("VAL",cardNumber.value, "SELECTED", selectedStack)
     const isAllowed = TheGame.isCardAllowed(cardNumber.value, selectedStack);
-    if(isAllowed){
-      selectedStack.cards.push(cardNumber.value)
+    if (isAllowed) {
+      selectedStack.cards.push(cardNumber.value);
       //remove card from players hand
-      console.log("Player Cards", game.players[0].cards, cardNumber.value)
-      const newUserCards = game.players[0].cards.filter((num: number) => num !== cardNumber.value);
+      console.log("Player Cards", game.players[0].cards, cardNumber.value);
+      const newUserCards = game.players[0].cards.filter(
+        (num: number) => num !== cardNumber.value
+      );
       console.log(newUserCards);
       game.players[0].cards = newUserCards;
-      console.log(game.players[0].cards)
+      console.log(game.players[0].cards);
       /** SET NEW GAME STORE */
       setGameStore(game);
     } else {
-      let newBtnStatus = {...buttonStatus}
+      let newBtnStatus = { ...buttonStatus };
       newBtnStatus[stackId].className = "btn-danger";
       setButtonStatus(newBtnStatus);
-      setTimeout(()=>{
-        newBtnStatus = {...buttonStatus}
+      setTimeout(() => {
+        newBtnStatus = { ...buttonStatus };
         newBtnStatus[stackId].className = "btn-light";
         setButtonStatus(newBtnStatus);
       }, 1500);
     }
-    
-  }
+  };
   return (
     <div className="card-wrapper m-2 p-2 d-flex align-items-center flex-column">
       <div className="card-header shadow">
         <div className="btn-group" role="group" aria-label="Basic example">
-          <button type="button" className={buttonStatus[0].className+" btn btn-sm"} onClick={() =>{addCardToStack(0)}}>A</button>
-          <button type="button" className={buttonStatus[1].className+" btn btn-sm"} onClick={() =>{addCardToStack(1)}}>B</button>
-          <button type="button" className={buttonStatus[2].className+" btn btn-sm"} onClick={() =>{addCardToStack(2)}}>C</button>
-          <button type="button" className={buttonStatus[3].className+" btn btn-sm"} onClick={() =>{addCardToStack(3)}}>D</button>
+          <button
+            type="button"
+            className={buttonStatus[0].className + " btn btn-sm"}
+            onClick={() => {
+              addCardToStack(0);
+            }}
+          >
+            A
+          </button>
+          <button
+            type="button"
+            className={buttonStatus[1].className + " btn btn-sm"}
+            onClick={() => {
+              addCardToStack(1);
+            }}
+          >
+            B
+          </button>
+          <button
+            type="button"
+            className={buttonStatus[2].className + " btn btn-sm"}
+            onClick={() => {
+              addCardToStack(2);
+            }}
+          >
+            C
+          </button>
+          <button
+            type="button"
+            className={buttonStatus[3].className + " btn btn-sm"}
+            onClick={() => {
+              addCardToStack(3);
+            }}
+          >
+            D
+          </button>
         </div>
       </div>
       <div className="user-card rounded-2 shadow py-4 m-2 d-flex justify-content-center align-items-center">
-        <span className="h1 fw-bolder m-0 p-4">{cardNumber.value}</span></div>
+        <span className="h1 fw-bolder m-0 p-4">{cardNumber.value}</span>
+      </div>
     </div>
-    );
+  );
 }
